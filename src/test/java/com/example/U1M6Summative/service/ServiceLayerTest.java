3c6c561a29b7026e8469a2f09b20618f2c03e5a1
JoeCeran <JoeCeran.Joecerankowski@gmail.com>
2019-07-09 15:19:32 -0400

    changed

--- src/test/java/com/example/U1M6Summative/service/ServiceLayerTest.java
+++ src/test/java/com/example/U1M6Summative/service/ServiceLayerTest.java
@@ -1,10 +1,9 @@
 package com.example.U1M6Summative.service;
 
-import com.example.U1M6Summative.dao.CustomerDao;
-import com.example.U1M6Summative.dao.InvoiceDao;
-import com.example.U1M6Summative.dao.InvoiceItemDao;
-import com.example.U1M6Summative.dao.ItemDao;
+import com.example.U1M6Summative.dao.*;
+import com.example.U1M6Summative.model.Item;
 import com.example.U1M6Summative.service.ServiceLayer;
+import com.example.U1M6Summative.viewmodel.ItemViewModel;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -30,10 +29,91 @@ public class ServiceLayerTest {
     public void setUp() throws Exception {
         setUpCustomerDaoMock();
         setUpInvoiceDaoMock();
-        setUpInvoiceDaoMock();
+        setUpInvoiceItemDaoMock();
         setUpItemDaoMock();
 
         service = new ServiceLayer(customerDao, invoiceDao, invoiceItemDao, itemDao);
+    }
 
+    //Item Tests
+
+    @Test
+    public void saveItem() {
+
+        Item item = new Item();
+
+        item.setName("book");
+        item.setDescription("romance book");
+        item.setDailyRate(new BigDecimal(7.76));
+
+        service.saveItem(item);
+
+        Item fromService = service.findItem(item.getId());
+
+        assertEquals(item, fromService);
+    }
+
+    @Test
+    public void findItem() {
+
+        Item item = service.findItem(0);
+        Item item2 = service.findItem(item.getId());
+
+        assertEquals(item, item2);
+    }
+
+    @Test
+    public void findAllItems() {
+
+        List<Item> fromService = service.findAllItems();
+
+        assertEquals(1, fromService.size());
+    }
+
+    @Test
+    public void updateItem() {
+
+    }
+
+    @Test
+    public void removeItem() {
+
+    }
+
+    //Set Ups
+
+    private void setUpCustomerDaoMock() {
+
+    }
+
+    private void setUpInvoiceDaoMock() {
+
+    }
+
+    private void setUpInvoiceItemDaoMock() {
+
+    }
+
+    private void setUpItemDaoMock() {
+        itemDao = mock(ItemDaoJdbcTemplateImpl.class);
+        Item item = new Item();
+        item.setName("book");
+        item.setDescription("romance book");
+        item.setDailyRate(new BigDecimal(7.76));
+
+
+        Item item2 = new Item();
+        item.setName("book2");
+        item.setDescription("fiction book");
+        item.setDailyRate(new BigDecimal(7.76));
+        item = itemDao.addItem(item);
+
+        List<Item> iList = new ArrayList<>();
+        iList.add(item);
+
+        doReturn(item).when(itemDao).addItem(item2);
+        doReturn(item).when(itemDao).getItem(1);
+        doReturn(iList).when(itemDao).getAllItems();
+        //doReturn(iList).when(itemDao).getItemsByCustomer(1);
     }
 }
